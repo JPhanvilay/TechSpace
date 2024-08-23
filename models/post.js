1700e8077ecc515f9eded4d5af1756c2eba46952
Jackson Phanvilay <jphanvilay.1@gmail.com>
2024-08-22 22:12:24 -0500

    Added starter code must be updated

---
+++ models/post.js
@@ -0,0 +1,28 @@
+const {Model, DataType} = require("sequelize");
+
+class Post extends Model {}
+
+Post.init(
+    {
+    title:{
+        type: DataType.STRING,
+        allowNull: false,
+    },
+    text: {
+       type: DataType.STRING,
+       allowNull: false, 
+    },
+    user_id: {
+        refrences: {
+            key: "id",
+            
+            model: "user",
+        }
+    },
+  },
+  {
+    freezeTableName: false,
+    underscored: true, 
+    modelName: "post",
+  },
+);
